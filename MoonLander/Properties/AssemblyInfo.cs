415382b1c50a051ef146dd69dee78078dbd71328
Robert Jurjevic <72772239+RJurjevic@users.noreply.github.com>
2025-08-02 15:37:12 +0100

    Initial commit: MoonLander simulation source code
    - C# console application simulating a Moon landing scenario
    - Inspired by the HP-34C programmable calculator moon lander program
    - Includes user-controlled fuel burn per time step
    - Uses Vis Viva-based exact interaction and impact energy calculations
    - Applies Newtonian dynamics, with conceptual support for geodesic motion (Vis Viva/Einstinian framework)
    - Outputs both exact and approximate collision energies
    - Displays total delivered interaction energy from fuel burns
    - Validates initial conditions (e.g., initial velocity must be downward or zero)
    - Supports adjustable time step and .NET Framework version (currently set to 4.7.2)

---
+++ MoonLander/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Vis Viva Moon Lander")]
+[assembly: AssemblyDescription("A lunar descent simulation inspired by HP-34C and powered by Vis Viva physics.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("M&R Research Team")]
+[assembly: AssemblyProduct("Vis Viva Moon Lander")]
+[assembly: AssemblyCopyright("Copyright © M&R Research 2025")]
+[assembly: AssemblyTrademark("Vis Viva")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9890b22b-02ca-4e81-8597-e7076a7ca882")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
